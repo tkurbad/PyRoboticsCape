b5b4102e3890ed09d81ef4466d83809ea6d3436a
Torsten Kurbad <github@tk-webart.de>
2017-06-02 21:42:22 +0200

    Changed license to MIT / Added setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+try:
+    from setuptools import setup, Extension
+except ImportError:
+    from distutils.core import setup, Extension
+
+setup(
+    name="roboticscape",
+	version="0.1",
+	description="Python bindings for libroboticscape",
+	author="Torsten Kurbad",
+	author_email="beaglebone@tk-webart.de",
+	maintainer="Torsten Kurbad",
+	maintainer_email="beaglebone@tk-webart.de",
+	license="MIT",
+	url="https://github.com/StrawsonDesign/Robotics_Cape_Installer",
+	ext_modules=[Extension("roboticscape", ["roboticscape.c"])])
