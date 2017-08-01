69f03b0a14ba56293ded6567d232cc945310431d
Torsten Kurbad <t.kurbad@iwm-kmrc.de>
2017-08-01 18:14:57 +0200

    Added convenient Python method

--- src/roboticscape/__init__.py
+++ src/roboticscape/__init__.py
@@ -4,3 +4,15 @@
 #
 
 from _roboticscape import *
+
+# Return values of rcGetState(), input to rcSetState()
+STATE = dict(
+    UNINITIALIZED   = 0
+    RUNNING         = 1
+    PAUSED          = 2
+    EXITING         = 3
+)
+
+def rcGetStateAsName():
+    state = rcGetState()
+    return list(STATE.keys())[list(STATE.values()).index(state)]
