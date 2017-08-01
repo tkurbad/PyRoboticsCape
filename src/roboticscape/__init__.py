94d869968eea85f81d77620ca3b9c6fc8a0e1b3a
Torsten Kurbad <t.kurbad@iwm-kmrc.de>
2017-08-01 17:51:36 +0200

    Try to fix naming weirdness

--- src/roboticscape/__init__.py
+++ src/roboticscape/__init__.py
@@ -3,12 +3,4 @@
 # Copyright (C) 2017 Torsten Kurbad <beaglebone@tk-webart.de>
 #
 
-# Bootstrap
-def __bootstrap__():
-    global __bootstrap__, __loader__, __file__
-    import sys, pkg_resources, imp
-    __file__ = pkg_resources.resource_filename(__name__, 'roboticscape.cpython-34m.so')
-    __loader__ = None; del __bootstrap__, __loader__
-    imp.load_dynamic(__name__, __file__)
-
-__bootstrap__()
+import _roboticscape
