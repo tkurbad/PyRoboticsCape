011d0cfbb452720ec7964d6b7aed2e0a4ef3f52a
Torsten Kurbad <github@tk-webart.de>
2017-08-11 21:24:14 +0200

    Try and fix passing of data

--- src/roboticscape/_roboticscapemodule.c
+++ src/roboticscape/_roboticscapemodule.c
@@ -924,7 +924,7 @@ static PyObject *rcReadI2CBytes(PyObject *self, PyObject *args) {
 
     retval = rc_i2c_read_bytes(bus, (uint8_t)address, (uint8_t)length, data);
 
-    printf("read %s\n", data);
+    printf("read %s\n", (char*)data);
 
     if (retval < 0) {
         PyErr_SetString(PyExc_ValueError, "Reading several bytes from I²C device failed.");
